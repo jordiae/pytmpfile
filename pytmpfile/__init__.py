92123029dbfbac36762f4b429f2a05e6d6853fba
jordiae <me@jordiae.com>
2022-03-10 17:26:45 +0000

    0.1.8

--- pytmpfile/__init__.py
+++ pytmpfile/__init__.py
@@ -1,16 +1,20 @@
 from memory_tempfile import MemoryTempfile
 import contextlib
 from typing import Optional
+import os
 
 __all__ = ['get_tmp_path', 'tempdir']
-__version__ = '0.1.6'
+__version__ = '0.1.8'
+
+SCRATCH_TMP = os.path.join('/scratch', 'tmp', os.environ['SLURM_JOB_ID']) if 'SLURM_JOB_ID' in os.environ else None
+PREF_PATH = [SCRATCH_TMP] if SCRATCH_TMP and os.path.exists(SCRATCH_TMP) else []
 
 # Credits: https://pypi.org/project/memory-tempfile/
-tempfile = MemoryTempfile(fallback=True)#(preferred_paths=['/run/user/{uid}'], remove_paths=['/dev/shm', '/run/shm'],
-#                          additional_paths=['/var/run'], filesystem_types=['tmpfs'], fallback=True)
+tempfile = MemoryTempfile(preferred_paths=PREF_PATH + ['/run/user/{uid}'],
+                          filesystem_types=['tmpfs', 'ramfs', 'xfs'], fallback=True)
 
-#if not tempfile.found_mem_tempdir():
-#    raise RuntimeError('No tmp directory found')
+if not tempfile.found_mem_tempdir():
+    raise RuntimeError('No tmp directory found')
 
 
 def tempdir():
